07aade272001e88f4a64cdf597994b4f4763cb33
Eduardo Jesus Berzunza Leon <eduardoberzunzal@gmail.com>
2021-07-25 11:52:31 -0500

    updating our application with reselect, replaced cart selectors with memorized selector

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -10,13 +10,10 @@ const CollectionItem = ({ item, addItem }) => {
   const { name, price, imageUrl } = item;
   return (
     <div className="collection-item">
-      <div
-        className="image"
-        style={{ backgroundImage: `url(${item.imageUrl})` }}
-      />
+      <div className="image" style={{ backgroundImage: `url(${imageUrl})` }} />
       <div className="collection-footer">
-        <span className="name">{item.name}</span>
-        <span className="price">{item.price}</span>
+        <span className="name">{name}</span>
+        <span className="price">{price}</span>
       </div>
       <CustomButton onClick={() => addItem(item)} inverted>
         Add to cart
